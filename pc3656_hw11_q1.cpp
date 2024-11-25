afa40645071fcd095c0821233fe52ebcbd9d07c5
sheep <pierre.coric.lg@gmail.com>
2024-11-24 19:15:15 -0500

    q1 ruler easy

--- pc3656_hw11_q1.cpp
+++ pc3656_hw11_q1.cpp
@@ -5,8 +5,9 @@
 using namespace std;
 
 void printTriangle(int n);
-void printLine(int n);
+void printLine(int n, char c);
 void printOppositeTriangle(int n);
+void printRuler(int n);
 
 int main() {
     //Testing a and b.
@@ -15,33 +16,35 @@ int main() {
         cout << "enter a positive integer: ";
         cin >> input;
     }
-    cout << "Testing q1 a with input " << input << endl;
+    cout << endl << "Testing q1 a with input " << input << endl;
     printTriangle(input);
-    cout << "Testing q1 b with input " << input << endl;
+    cout << endl << "Testing q1 b with input " << input << endl;
     printOppositeTriangle(input);
+    cout << endl << "Testing q1 c with input " << input << endl;
+    printRuler(input);
     return 0;
 }
 
 //Recursive function to print a line of n *.
-void printLine(int n) {
+void printLine(int n, char c) {
     if(n == 1) {
-        cout << '*';
+        cout << c;
     }
     else {
-        printLine(n - 1);
-        cout << "*"; 
+        printLine(n - 1, c);
+        cout << c; 
     }
 } 
 
 //q1-a
 void printTriangle(int n) {
     if(n == 1) {
-        printLine(n);
+        printLine(n, '*');
         cout << endl;
     }
     else {
         printTriangle(n - 1);
-        printLine(n);
+        printLine(n, '*');
         cout << endl; 
     }
 }
@@ -49,16 +52,31 @@ void printTriangle(int n) {
 //q1-b
 void printOppositeTriangle(int n) {
     if(n == 1) {
-        printLine(n);
+        printLine(n, '*');
         cout << endl;
-        printLine(n);
+        printLine(n, '*');
         cout << endl;
     }
     else {
-        printLine(n);
+        printLine(n, '*');
         cout << endl; 
         printOppositeTriangle(n - 1);
-        printLine(n);
+        printLine(n, '*');
         cout << endl;
     }
+}
+
+//q1-c less scary than it looks.
+void printRuler(int n) {
+    if(n == 1) {
+        printLine(n, '-');
+        cout << endl;
+    }
+    else {
+        printRuler(n - 1);
+        printLine(n, '-');
+        cout << endl; 
+        printRuler(n - 1);
+    }
+
 }
